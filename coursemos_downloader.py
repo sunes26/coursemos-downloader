1e62ae4fccb891e8076b6c5bcb7d818f648ce630
sunes26 <85713817+sunes26@users.noreply.github.com>
2025-03-15 19:30:36 +0900

    Update coursemos_downloader.py

--- coursemos_downloader.py
+++ coursemos_downloader.py
@@ -22,7 +22,7 @@ except ImportError:
     sys.exit(1)
 
 # 앱 버전 정보
-APP_VERSION = "1.0.16"
+APP_VERSION = "1.0.17"
 GITHUB_OWNER = "sunes26"  # 여기에 GitHub 사용자명 입력
 GITHUB_REPO = "coursemos-downloader"  # 저장소 이름
 
